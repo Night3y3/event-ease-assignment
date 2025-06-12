7f4605f96f176c0e0ce135eae58e0717c52ba682
Night3y3 <sabrexghosh@gmail.com>
2025-06-12 22:44:58 +0530

    padding for create fixed

--- app/dashboard/events/new/page.tsx
+++ app/dashboard/events/new/page.tsx
@@ -6,13 +6,12 @@ export default async function NewEventPage() {
   const session = await getServerAuthSession()
 
   return (
-    <div className="space-y-6 max-w-screen-md w-full mx-auto px-4 sm:px-6">
+    <div className="max-w-2xl w-full mx-auto px-4 sm:px-6 space-y-6">
       <DashboardHeader
         heading="Create Event"
         text="Create a new event and publish it to your audience."
       />
       <EventForm userId={session?.user?.id} />
     </div>
-
   )
 }
